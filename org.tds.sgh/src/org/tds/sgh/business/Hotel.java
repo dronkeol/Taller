f21de6103bbfebd8390a5a5ccb3b143618c7d357
Cristian Alballay S. <cristian.alballay@gmail.com>
2015-08-08 10:40:00 -0400

    Se agrega mapa de reservas y metodo que devuelve un Stream para obtener una lista de ellas

--- org.tds.sgh/src/org/tds/sgh/business/Hotel.java
+++ org.tds.sgh/src/org/tds/sgh/business/Hotel.java
@@ -4,57 +4,59 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Stream;
 
-public class Hotel
-{
-	// Attributes (private) -----------------------------------------------------------------------
-	
-	private String nombre;
-	
-	private String pais;
-	
-	private Map<String,Habitacion> habitaciones;
-	
-	
-	// Constructors (public) ----------------------------------------------------------------------
-	
-	public Hotel(String nombre, String pais)
-	{
-		this.nombre = nombre;
-		
-		this.pais = pais;
-		
-		this.habitaciones = new HashMap<String,Habitacion>();
-	}
-	
-	
-	// Properties (public) ------------------------------------------------------------------------
-	
-	public String getNombre()
-	{
-		return nombre;
-	}
-	
-	public String getPais()
-	{
-		return pais;
-	}
-	
-	
-	// Operations (public) ------------------------------------------------------------------------
-	
-	public Habitacion agregarHabitacion(TipoHabitacion tipoHabitacion, String nombre) throws Exception
-	{
-		if (habitaciones.containsKey(nombre)) throw new Exception("El hotel ya tiene una habitación con el nombre indicado.");
-		
-		Habitacion habitacion = new Habitacion(tipoHabitacion, nombre);
-		
-		habitaciones.put(habitacion.getNombre(), habitacion);
-		
-		return habitacion;
-	}
-	
-	public Stream<Habitacion> listarHabitaciones()
-	{
-		return habitaciones.values().stream();
-	}
+public class Hotel {
+  // Attributes (private) -----------------------------------------------------------------------
+
+  private String nombre;
+
+  private String pais;
+
+  private Map<String, Habitacion> habitaciones;
+
+  private Map<Integer, Reserva> reservas;
+
+
+  // Constructors (public) ----------------------------------------------------------------------
+
+  public Hotel(String nombre, String pais) {
+    this.nombre = nombre;
+
+    this.pais = pais;
+
+    this.habitaciones = new HashMap<String, Habitacion>();
+  }
+
+
+  // Properties (public) ------------------------------------------------------------------------
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public String getPais() {
+    return pais;
+  }
+
+
+  // Operations (public) ------------------------------------------------------------------------
+
+  public Habitacion agregarHabitacion(TipoHabitacion tipoHabitacion, String nombre)
+      throws Exception {
+    if (habitaciones.containsKey(nombre))
+      throw new Exception("El hotel ya tiene una habitaciï¿½n con el nombre indicado.");
+
+    Habitacion habitacion = new Habitacion(tipoHabitacion, nombre);
+
+    habitaciones.put(habitacion.getNombre(), habitacion);
+
+    return habitacion;
+  }
+
+  public Stream<Habitacion> listarHabitaciones() {
+    return habitaciones.values().stream();
+  }
+
+  public Stream<Reserva> listarReservas() {
+    return reservas.values().stream();
+  }
 }
