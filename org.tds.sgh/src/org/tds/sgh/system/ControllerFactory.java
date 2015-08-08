e0d45ad008ee61f6e7d5b908161fc471bb633636
Cristian Alballay S. <cristian.alballay@gmail.com>
2015-08-08 10:19:09 -0400

    Se agrega constructor public a HacerReservaController que recibe por parametro una cadena hotelera.
    Se modifica metodo en clase factory para que devuelva un objeto
    HacerReservaController

--- org.tds.sgh/src/org/tds/sgh/system/ControllerFactory.java
+++ org.tds.sgh/src/org/tds/sgh/system/ControllerFactory.java
@@ -3,53 +3,45 @@ package org.tds.sgh.system;
 import org.tds.sgh.business.CadenaHotelera;
 import org.tds.sgh.infrastructure.NotImplementedException;
 
-public class ControllerFactory implements IControllerFactory
-{
-	// Attributes (private) -----------------------------------------------------------------------
-	
-	private CadenaHotelera cadenaHotelera;
-	
-	
-	// Constructors (public) ----------------------------------------------------------------------
-	
-	public ControllerFactory(CadenaHotelera cadenaHotelera)
-	{
-		this.cadenaHotelera = cadenaHotelera;
-	}
+public class ControllerFactory implements IControllerFactory {
+  // Attributes (private) -----------------------------------------------------------------------
+
+  private CadenaHotelera cadenaHotelera;
 
 
-	// Operations (public) ------------------------------------------------------------------------
+  // Constructors (public) ----------------------------------------------------------------------
 
-	@Override
-	public ICadenaController createCadenaController()
-	{
-		return new CadenaController(cadenaHotelera);
-	}
-	
-	@Override
-	public IHacerReservaController createHacerReservaController()
-	{
-		// TODO
-		throw new NotImplementedException();
-	}
-	
-	@Override
-	public IModificarReservaController createModificarReservaController()
-	{
-		// TODO
-		throw new NotImplementedException();
-	}
-	
-	@Override
-	public ICancelarReservaController createCancelarReservaController()
-	{
-		// TODO
-		throw new NotImplementedException();
-	}
+  public ControllerFactory(CadenaHotelera cadenaHotelera) {
+    this.cadenaHotelera = cadenaHotelera;
+  }
 
-	public ITomarReservaController createTomarReservaController()
-	{
-		// TODO
-		throw new NotImplementedException();
-	}
+
+  // Operations (public) ------------------------------------------------------------------------
+
+  @Override
+  public ICadenaController createCadenaController() {
+    return new CadenaController(cadenaHotelera);
+  }
+
+  @Override
+  public IHacerReservaController createHacerReservaController() {
+    return new HacerReservaController(cadenaHotelera);
+  }
+
+  @Override
+  public IModificarReservaController createModificarReservaController() {
+    // TODO
+    throw new NotImplementedException();
+  }
+
+  @Override
+  public ICancelarReservaController createCancelarReservaController() {
+    // TODO
+    throw new NotImplementedException();
+  }
+
+  public ITomarReservaController createTomarReservaController() {
+    // TODO
+    throw new NotImplementedException();
+  }
 }
--- org.tds.sgh/src/org/tds/sgh/system/HacerReservaController.java
+++ org.tds.sgh/src/org/tds/sgh/system/HacerReservaController.java
@@ -1,8 +1,15 @@
 package org.tds.sgh.system;
 
+import org.tds.sgh.business.CadenaHotelera;
+
 
 
 public class HacerReservaController extends TomarReservaController implements
     IHacerReservaController {
 
+  private CadenaHotelera cadenaHotelera;
+
+  public HacerReservaController(CadenaHotelera cadenaHotelera) {
+    this.cadenaHotelera = cadenaHotelera;
+  }
 }
