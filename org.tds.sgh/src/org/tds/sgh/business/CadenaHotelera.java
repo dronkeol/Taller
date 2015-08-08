f7124456c062158c5e0a144b267c64e7125a4d55
Cristian Alballay S. <cristian.alballay@gmail.com>
2015-08-08 11:57:25 -0400

    Se agrega metodo confirmaDisponibilidad

--- org.tds.sgh/src/org/tds/sgh/business/CadenaHotelera.java
+++ org.tds.sgh/src/org/tds/sgh/business/CadenaHotelera.java
@@ -1,124 +1,153 @@
 package org.tds.sgh.business;
 
+import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Stream;
 
-public class CadenaHotelera
-{
-	// Attributes (private) -----------------------------------------------------------------------
-	
-	private String nombre;
+import org.tds.sgh.infrastructure.Infrastructure;
 
-	private Map<String,Cliente> clientes;
-	
-	private Map<String,Hotel> hoteles;
-	
-	private Map<String,TipoHabitacion> tiposHabitacion;
-	
-	
-	// Constructors (public) ----------------------------------------------------------------------
+public class CadenaHotelera {
+  // Attributes (private) -----------------------------------------------------------------------
 
-	public CadenaHotelera(String nombre)
-	{
-		this.nombre = nombre;
-		
-		this.clientes = new HashMap<String,Cliente>();
-		
-		this.hoteles = new HashMap<String,Hotel>();
-		
-		this.tiposHabitacion = new HashMap<String,TipoHabitacion>();
-	}
-	
-	
-	// Properties (public) ------------------------------------------------------------------------
+  private String nombre;
 
-	public String getNombre()
-	{
-		return nombre;
-	}
+  private Map<String, Cliente> clientes;
 
-	
-	// Operations (public) ------------------------------------------------------------------------
-	
-	public Cliente agregarCliente(String rut, String nombre, String direccion, String telefono, String mail) throws Exception
-	{
-		if (clientes.containsKey(rut)) throw new Exception("Ya existe un cliente con el RUT indicado.");
-		
-		Cliente cliente = new Cliente(rut, nombre, direccion, telefono, mail);
-		
-		clientes.put(cliente.getRut(), cliente);
-		
-		return cliente;
-	}
+  private Map<String, Hotel> hoteles;
 
-	public Cliente buscarCliente(String rut) throws Exception
-	{
-		Cliente cliente = clientes.get(rut);
-		
-		if (cliente == null) throw new Exception("No existe un cliente con el nombre indicado.");
-		
-		return cliente;
-	}
+  private Map<String, TipoHabitacion> tiposHabitacion;
 
-	public Stream<Cliente> buscarClientes(String patronNombreCliente)
-	{
-		return clientes.values().stream()
-			.filter(cliente -> cliente.getNombre().matches(patronNombreCliente));
-	}
 
-	public Stream<Cliente> listarClientes()
-	{
-		return clientes.values().stream();
-	}
+  // Constructors (public) ----------------------------------------------------------------------
 
-	public Hotel agregarHotel(String nombre, String pais) throws Exception
-	{
-		if (hoteles.containsKey(nombre)) throw new Exception("Ya existe un hotel con el nombre indicado.");
-		
-		Hotel hotel = new Hotel(nombre, pais);
-		
-		hoteles.put(hotel.getNombre(), hotel);
-		
-		return hotel;
-	}
+  public CadenaHotelera(String nombre) {
+    this.nombre = nombre;
 
-	public Hotel buscarHotel(String nombre) throws Exception
-	{
-		Hotel hotel = hoteles.get(nombre);
-		
-		if (hotel == null) throw new Exception("No existe un hotel con el nombre indicado.");
-		
-		return hotel;
-	}
-	
-	public Stream<Hotel> listarHoteles()
-	{
-		return hoteles.values().stream();
-	}
+    this.clientes = new HashMap<String, Cliente>();
 
-	public TipoHabitacion agregarTipoHabitacion(String nombre) throws Exception
-	{
-		if (tiposHabitacion.containsKey(nombre)) throw new Exception("Ya existe un tipo de habitación con el nombre indicado.");
-		
-		TipoHabitacion tipoHabitacion = new TipoHabitacion(nombre);
-		
-		tiposHabitacion.put(tipoHabitacion.getNombre(), tipoHabitacion);
-		
-		return tipoHabitacion;
-	}
+    this.hoteles = new HashMap<String, Hotel>();
 
-	public TipoHabitacion buscarTipoHabitacion(String nombre) throws Exception
-	{
-		TipoHabitacion tipoHabitacion = tiposHabitacion.get(nombre);
-		
-		if (tipoHabitacion == null) throw new Exception("No existe un tipo de habitación con el nombre indicado.");
-		
-		return tipoHabitacion;
-	}
-	
-	public Stream<TipoHabitacion> listarTiposHabitacion()
-	{
-		return tiposHabitacion.values().stream();
-	}
+    this.tiposHabitacion = new HashMap<String, TipoHabitacion>();
+  }
+
+
+  // Properties (public) ------------------------------------------------------------------------
+
+  public String getNombre() {
+    return nombre;
+  }
+
+
+  // Operations (public) ------------------------------------------------------------------------
+
+  public Cliente agregarCliente(String rut, String nombre, String direccion, String telefono,
+      String mail) throws Exception {
+    if (clientes.containsKey(rut))
+      throw new Exception("Ya existe un cliente con el RUT indicado.");
+
+    Cliente cliente = new Cliente(rut, nombre, direccion, telefono, mail);
+
+    clientes.put(cliente.getRut(), cliente);
+
+    return cliente;
+  }
+
+  public Cliente buscarCliente(String rut) throws Exception {
+    Cliente cliente = clientes.get(rut);
+
+    if (cliente == null)
+      throw new Exception("No existe un cliente con el nombre indicado.");
+
+    return cliente;
+  }
+
+  public Stream<Cliente> buscarClientes(String patronNombreCliente) {
+    return clientes.values().stream()
+        .filter(cliente -> cliente.getNombre().matches(patronNombreCliente));
+  }
+
+  public Stream<Cliente> listarClientes() {
+    return clientes.values().stream();
+  }
+
+  public Hotel agregarHotel(String nombre, String pais) throws Exception {
+    if (hoteles.containsKey(nombre))
+      throw new Exception("Ya existe un hotel con el nombre indicado.");
+
+    Hotel hotel = new Hotel(nombre, pais);
+
+    hoteles.put(hotel.getNombre(), hotel);
+
+    return hotel;
+  }
+
+  public Hotel buscarHotel(String nombre) throws Exception {
+    Hotel hotel = hoteles.get(nombre);
+
+    if (hotel == null)
+      throw new Exception("No existe un hotel con el nombre indicado.");
+
+    return hotel;
+  }
+
+  public Stream<Hotel> listarHoteles() {
+    return hoteles.values().stream();
+  }
+
+  public TipoHabitacion agregarTipoHabitacion(String nombre) throws Exception {
+    if (tiposHabitacion.containsKey(nombre))
+      throw new Exception("Ya existe un tipo de habitaciï¿½n con el nombre indicado.");
+
+    TipoHabitacion tipoHabitacion = new TipoHabitacion(nombre);
+
+    tiposHabitacion.put(tipoHabitacion.getNombre(), tipoHabitacion);
+
+    return tipoHabitacion;
+  }
+
+  public TipoHabitacion buscarTipoHabitacion(String nombre) throws Exception {
+    TipoHabitacion tipoHabitacion = tiposHabitacion.get(nombre);
+
+    if (tipoHabitacion == null)
+      throw new Exception("No existe un tipo de habitaciï¿½n con el nombre indicado.");
+
+    return tipoHabitacion;
+  }
+
+  public Stream<TipoHabitacion> listarTiposHabitacion() {
+    return tiposHabitacion.values().stream();
+  }
+
+  public boolean confirmaDisponibilidad(String nombreHotel, String nombreTipoHabitacion,
+      GregorianCalendar fechaInicio, GregorianCalendar fechaFin) {
+
+    Hotel oHotel = null;
+    try {
+      oHotel = this.buscarHotel(nombreHotel);
+    } catch (Exception e) {
+      return false;
+    }
+
+    Stream<Habitacion> lstHabitaciones =
+        oHotel.listarHabitaciones().filter(
+            h -> h.getTipoHabitacion().getNombre().equals(nombreTipoHabitacion));
+    long countHabitaciones = lstHabitaciones.count();
+
+    Stream<Reserva> lstReservas =
+        oHotel.listarReservas().filter(
+            p -> p.getTipoHabitacion().getNombre().equals(nombreTipoHabitacion)
+
+                && EstadoReserva.PENDIENTE.equals(p.getEstado())
+                && Infrastructure.getInstance().getCalendario()
+                    .esPosterior(p.getFechaInicio(), fechaInicio)
+                && Infrastructure.getInstance().getCalendario()
+                    .esAnterior(p.getFechaFin(), fechaFin)
+
+
+
+        );
+    long countReservas = lstReservas.count();
+    return countHabitaciones > countReservas;
+  }
 }
