6591085f5c6dfc1d2c544d5719d4b9362a4159f9
Ricardo Gómez <buchardo@gmail.com>
2015-08-08 18:36:26 -0300

    Se agregan Controllers

---
+++ org.tds.sgh/src/org/tds/sgh/system/IdentificarClienteEnRecepcionController.java
@@ -0,0 +1,20 @@
+package org.tds.sgh.system;
+
+import java.util.List;
+
+import org.tds.sgh.dtos.ClienteDTO;
+
+public class IdentificarClienteEnRecepcionController implements IIdentificarClienteEnRecepcionController {
+
+	@Override
+	public List<ClienteDTO> buscarCliente(String patronNombreCliente) {
+		return null;
+	}
+
+	@Override
+	public ClienteDTO seleccionarCliente(String rut) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
